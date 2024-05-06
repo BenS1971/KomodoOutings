9bd905a97fc3e22e1ef1f834edb20a28243b94ee
Ben Strycker <bgstrycker1971@gmail.com>
2024-05-05 20:16:34 -0400

    More changes on a Sunday evening...

--- OutingsRepo/OutingsRepository.cs
+++ OutingsRepo/OutingsRepository.cs
@@ -2,7 +2,7 @@ namespace OutingsRepo.repository;
 
 public class OutingsRepository
 {
-    private List<Outings> _outings = new List<Outings>();
+    private List<Outings> _listOfOutings = new List<Outings>();
     
     //Create outings...
     public void  AddContentToList(Outings outings)
@@ -10,20 +10,59 @@ public class OutingsRepository
         _outings.Add(outings);
     }
     //Read outings...
-    public List<Outings> GetOutingsList();
+    public List<Outings> GetOutingList(Outings);
     {
-        return new List<Outings>(_ListOfOutings);
+        return new List<Outings>(_listOfOutings);
     }
 
     // Update outings...
+    public bool UpdateExistingOutings(string outings, Outings newOutings)
+    {
+        //Find that outings content...
+        Outings oldContent = GetOutingList(outings);
+
+        //...And then UPDATE that outing content. Okay? Okay.
+        if (oldContent != null)
+        {
+            oldContent.TypeOfOuting = newOutings.TypeOfOuting;
+            oldContent.OutingDate = newOutings.OutingDate;
+            oldContent.CostPerPerson = newOutings.CostPerPerson;
+            oldContent.TotalCostOfOuting = newOutings.TotalCostOfOuting;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        }
+    
 
     // Delete them outings (Bye bye.)...
-    public bool RemoveOutingsFromList()
+    public bool RemoveOutingsFromList(string outings)
     {
+        Outings content = GetOutingList();
+        if (content == null)
+        {
+            return false;
+        }
 
+        int initial = listOfOutings.Count;
+        _listOfContent.Remove(content);
+
+        if (initialCount > listOfOutings.Count)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+    
     //Helper Methods (NOT tuna or hamburger!!)
-    private Outings GetOutingsFromList(string outings)
+    private Outings? GetOutingsFromList(string outings)
     {
         foreach (Outings content in _listOfOutings)
         {
@@ -32,5 +71,6 @@ public class OutingsRepository
                 return content;
             }
         }
+        return null;
     }
 }
